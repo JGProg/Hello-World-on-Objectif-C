e9646832947e2143c3a11879a1e3384fa02fc6de
lefandordinateur <jessy.giacomoni@me.com>
2013-06-27 22:11:32 +0200

    Add navigation bar Add Toolbar Add Facebook Share Button

--- HellowWorld/HelloWorldViewController.m
+++ HellowWorld/HelloWorldViewController.m
@@ -7,12 +7,18 @@
 //
 
 #import "HelloWorldViewController.h"
+#import "social/Social.h"
+#import "accounts/Accounts.h"
+
 
 @interface HelloWorldViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *Label;
 @property (weak, nonatomic) IBOutlet UITextField *textField;
+@property (weak, nonatomic) IBOutlet UIButton *ButtonEnter;
 
 - (IBAction)ChangeGreeting:(id)sender;
+
+- (IBAction)facebook:(id)sender;
 
 @end
 
@@ -21,7 +27,13 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    self.Label.text = @"I know to say Hello ;)";
+    self.textField.placeholder = @"Enter your name Here :D";
+    self.ButtonEnter.titleLabel.text = @"Push here when you have clicked :)";
+    self.ButtonEnter.enabled =false;
 	// Do any additional setup after loading the view, typically from a nib.
+
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -34,13 +46,15 @@
 {
     if (theTextField == self.textField) {
         [theTextField resignFirstResponder];
+        self.ButtonEnter.enabled =true;
     }
-    UIAlertView *message = [[UIAlertView alloc] initWithTitle:@"Hello World!"
+    /*UIAlertView *message = [[UIAlertView alloc] initWithTitle:@"Hello World!"
                                                       message:@"Non mais Hello World quoi."
                                                      delegate:self // Call : (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
                                             cancelButtonTitle:@"OK"
                                             otherButtonTitles:@"Button 2", @"Button 3", nil];
-    [message show];
+    [message show];*/
+    
     return YES;
 }
 
@@ -76,4 +90,45 @@
         NSLog(@"Button 3 was selected.");
     }
 }
+
+
+-(IBAction)facebook:(id)sender{
+    if([SLComposeViewController isAvailableForServiceType:SLServiceTypeFacebook]) {
+        
+        SLComposeViewController *controller = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeFacebook];
+        
+        SLComposeViewControllerCompletionHandler myBlock = ^(SLComposeViewControllerResult result){
+            if (result == SLComposeViewControllerResultCancelled) {
+                
+                NSLog(@"Cancelled");
+                
+            } else
+                
+            {
+                NSLog(@"Done");
+            }
+            
+            [controller dismissViewControllerAnimated:YES completion:Nil];
+        };
+        controller.completionHandler =myBlock;
+        
+        //Adding the Text to the facebook post value from iOS
+        [controller setInitialText:@"Ce site est vraiment trop bien ;)"];
+        
+        //Adding the URL to the facebook post value from iOS
+        [controller addURL:[NSURL URLWithString:@"http://www.jessygiacomoni.fr"]];
+        
+        //Adding the Text to the facebook post value from iOS
+        [controller addImage:[UIImage imageNamed:@"fb.png"]];
+        
+        [self presentViewController:controller animated:YES completion:Nil];
+        
+    }
+    else{
+        NSLog(@"UnAvailable");
+    }
+    
+}
+
+
 @end
