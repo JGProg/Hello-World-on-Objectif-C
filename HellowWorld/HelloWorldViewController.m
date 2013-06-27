f56531f46808836890081dd50449fb08a46f9106
lefandordinateur <jessy.giacomoni@me.com>
2013-06-27 15:31:42 +0200

    When clicked on button the label take label of textField

--- HellowWorld/HelloWorldViewController.m
+++ HellowWorld/HelloWorldViewController.m
@@ -9,6 +9,10 @@
 #import "HelloWorldViewController.h"
 
 @interface HelloWorldViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *Label;
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+
+- (IBAction)ChangeGreeting:(id)sender;
 
 @end
 
@@ -26,4 +30,24 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (BOOL)textFieldShouldReturn:(UITextField *)theTextField
+{
+    if (theTextField == self.textField) {
+        [theTextField resignFirstResponder];
+    }
+    return YES;
+}
+
+- (IBAction)ChangeGreeting:(id)sender
+{
+    self.userName = self.textField.text;
+   
+    NSString *nameString = self.userName;
+    if ([nameString length] == 0) {
+        nameString = @"World";
+    }
+    
+    NSString *greeting = [[NSString alloc] initWithFormat:@"Hello, %@!", nameString];
+    self.Label.text = greeting;
+}
 @end
